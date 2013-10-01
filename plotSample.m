25c0b51dd82ce40bc0a072c73af5202339637258
somussma <somussma@purdue.edu>
2013-09-30 22:00:05 -0400

    Change PlotSample

--- plotSample.m
+++ plotSample.m
@@ -2,12 +2,13 @@
 
 x=(-5:0.1:5)'; %this will be the range of x-values on the plot
 
-C = gcov(x,x,'c'); %set the covariance matrix
+C = gramcov(x,x,'p',[1 1 3]'); %set the covariance matrix
 
 numSamples=1; %this is the number of functions graphed
 
 figure; %plot the functions...
 for i=1:numSamples
+    t=mvnrnd(zeros(size(x)),C);
     hold on;
-    plot(x,mvnrnd(zeros(size(x)),C))
+    plot(x,t);
 end
